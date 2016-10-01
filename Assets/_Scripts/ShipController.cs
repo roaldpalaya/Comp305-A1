3f12496417c543be6910703f301f62c5a5e1fcde
roaldpalaya <roaldrussel@hotmail.com>
2016-10-01 17:55:26 -0400

    Initial Commit

---
+++ Assets/_Scripts/ShipController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipController : MonoBehaviour {
+
+    
+    private float _shipSpd = 50.0f;
+    private Transform _trfrm;
+
+    public float ShpSpeed
+    {
+        get
+        {
+            return this._shipSpd;
+        }
+        set
+        {
+            this._shipSpd = value;
+        }
+    }
+
+    // Current speed of the player
+    private float curSpeed = 0.0f;
+
+    // The last movement done
+    private Vector3 lastMovement = new Vector3();
+    // Use this for initialization
+    void Start () {
+        this._trfrm = this.GetComponent<Transform>();
+        this._shipSpd = 50.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        _movement();
+	}
+
+    //Where the horizontal movement is done
+    private void _movement()
+    {
+        Vector2 _move = new Vector2();
+        _move.x += Input.GetAxis("Horizontal");
+
+        _move.Normalize();
+
+        if (_move.magnitude > 0)
+        {
+            curSpeed = _shipSpd;
+            this.transform.Translate(_move * Time.deltaTime * _shipSpd, Space.World);
+            lastMovement = _move;
+        }
+        
+        Vector2 _pos = _trfrm.position;
+        _pos.x=Mathf.Clamp(this._trfrm.position.x, -270, 265);
+        this._trfrm.position = _pos;
+        
+    }
+}
---
+++ Assets/_Scripts/SpaceController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceController : MonoBehaviour {
+    private int _spd = 3;
+    private Transform _trfrm;
+
+    public int Spd {
+        get {
+            return this._spd;
+        }
+
+        set
+        {
+            this._spd = value;
+        }
+            
+    }
+     // Use this for initialization
+    void Start () {
+        this._trfrm = this.GetComponent<Transform>();
+        this._spd = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this._movement();
+        this._boundChk();
+	}
+
+    //moves the background
+    private void _movement()
+    {
+        Vector2 newPos = this._trfrm.position;
+        newPos.y -= this._spd;
+        this._trfrm.position = newPos;
+
+    }
+    //Check if background is almost out of bounds
+    private void _boundChk()
+    {
+        if(this._trfrm.position.y <= -309f)
+        {
+            this._reset();
+        }
+
+    }
+    //resets the background to starting point to scroll again
+    private void _reset()
+    {
+        this._trfrm.position = new Vector2(0f, 309f);
+
+    }
+}
